b4d29b9fcb4bdaae8b44482531bfff267510bbc3
mohammad <m.baniasadi.d@gmail.com>
2023-09-09 14:57:50 +0330

    add create task and goal and show-task urls and create the corresponding views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import home
+from .views import home, showTasks, createTask, createGoal
 
 
 urlpatterns = [
     path('', name='home', view=home),
+    path('show-tasks/', name='show-tasks', view=showTasks),
+    path('create-task/', name='create-task', view=createTask),
+    path('create-goal/', name='create-goal', view=createGoal),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 from .models import Task, Goal
+from .forms import TaskCreateForm, GoalCreateForm
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, template_name='tasks/home.html')
+    return render(request, template_name='home.html')
+
+def showTasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/show_tasks.html', {'tasks': tasks})
+
+def createTask(request):
+    form = TaskCreateForm()
+    if request.method == 'POST':
+        form = TaskCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'task create successfully')
+            return redirect(showTasks)
+    return render(request, 'tasks/create_task.html', {'form': form})
+
+def createGoal(request):
+    form = GoalCreateForm()
+    if request.method == 'POST':
+        form = GoalCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'goal create successfully')
+            return redirect(showTasks)
+    return render(request, 'tasks/create_goal.html', {'form': form})
