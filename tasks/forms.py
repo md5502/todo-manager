2d801d66891b56ea26f79dab4fbdac86af271ab0
mohammad <m.baniasadi.d@gmail.com>
2023-09-09 14:56:45 +0330

    edit forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,25 +1,28 @@
 from django import forms
 from .models import Task, Goal
 
-class CreateTaskForm(forms.ModelForm):
+class TaskCreateForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'goal', 'user', 'is_completed', 'importance_tag']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'goal': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'goal': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'user': forms.Select(attrs={'class': 'form-control'}),
             'is_completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-            'importance_tag': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'importance_tag': forms.Select(attrs={'class': 'form-control'}),
         }
 
 class GoalCreateForm(forms.ModelForm):
     class Meta:
         model = Goal
-        fields = ['title', 'description', 'user', 'is_completed', 'importance_tag']
+        fields = ['title', 'description', 'user', 'is_completed', 'importance_tag', 'death_line']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'user': forms.Select(attrs={'class': 'form-control'}),
             'is_completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-            'importance_tag': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'importance_tag': forms.Select(attrs={'class': 'form-control'}),
+            'death_line': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
         }
