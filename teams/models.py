137156c9a6b2fa6d7937f96cf9325542d9857421
mohammad <m.baniasadid.d@gmail.com>
2023-09-25 16:19:44 +0330

    create teams app and create the coresponding models

---
+++ teams/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import User
+# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    leaders = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+    id = models.CharField(max_length=255, unique=True, primary_key=True, default=uuid4, editable=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class TeamMember(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    assigned_members = models.ManyToManyField(User, related_name='tasks_assigned', blank=True)
+
+    id = models.CharField(max_length=255, unique=True, primary_key=True, default=uuid4, editable=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
