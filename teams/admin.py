ee33153728493fc12e8b29f2449c5b9043920516
mohammad <m.baniasadid.d@gmail.com>
2023-09-25 16:30:37 +0330

    migrate and register the models to admin.py

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Team, TeamMember
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+class TeamMemberAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(TeamMember, TeamMemberAdmin)
