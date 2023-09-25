2e690c8145048bf7b2766086acab1a9af6748c57
mohammad <m.baniasadid.d@gmail.com>
2023-09-25 13:10:17 +0330

    add the edit views for task and goal and create urls path

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import home, showTasks, createTask, createGoal
+from .views import home, showTasks, createTask, createGoal, editTask, editGoal
 
 
 urlpatterns = [
     path('', name='home', view=home),
     path('show-tasks/', name='show-tasks', view=showTasks),
     path('create-task/', name='create-task', view=createTask),
+    path('edit-task/<str:pk>', name='edit-task', view=editTask),
     path('create-goal/', name='create-goal', view=createGoal),
+    path('edit-goal/<str:pk>', name='edit-goal', view=editGoal),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
 from .models import Task, Goal
@@ -22,7 +22,25 @@ def createTask(request):
             form.save()
             messages.success(request, 'task create successfully')
             return redirect(showTasks)
-    return render(request, 'tasks/create_task.html', {'form': form})
+    return render(request, 'tasks/create_task.html', {'form': form,  'page' : 'create'})
+
+def editTask(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    
+    if task.user != request.user:
+        messages.error(request, "You don't have permission to edit this task.")
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = TaskCreateForm(request.POST, request.FILES, instance=task)
+        if form.is_valid():
+            form.save()
+            messages.info(request, "Task has been updated successfully.")
+            return redirect('home')
+    else:
+        form = TaskCreateForm(instance=task)
+
+    return render(request, template_name='tasks/create_task.html', context={'form': form, 'page' : 'edit', 'id':pk })
 
 def createGoal(request):
     form = GoalCreateForm()
@@ -33,3 +51,21 @@ def createGoal(request):
             messages.success(request, 'goal create successfully')
             return redirect(showTasks)
     return render(request, 'tasks/create_goal.html', {'form': form})
+
+def editGoal(request, pk):
+    task = get_object_or_404(Goal, id=pk)
+    
+    if task.user != request.user:
+        messages.error(request, "You don't have permission to edit this task.")
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = GoalCreateForm(request.POST, request.FILES, instance=task)
+        if form.is_valid():
+            form.save()
+            messages.info(request, "Task has been updated successfully.")
+            return redirect('home')
+    else:
+        form = GoalCreateForm(instance=task)
+
+    return render(request, template_name='tasks/create_goal.html', context={'form': form, 'page' : 'edit', 'id':pk})
